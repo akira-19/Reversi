ed64ce92c2a18955a2cab302b1f14818ae6c115c
akira-19 <dounatsu00@yahoo.co.jp>
2019-04-07 11:19:44 -0700

    Updates

---
+++ precache-manifest.62f878ec9e7e2978d82ea2411dc7171a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc1d8dd684dbff1a03d",
+    "url": "/Reversi/static/css/main.44625df5.chunk.css"
+  },
+  {
+    "revision": "5fc1d8dd684dbff1a03d",
+    "url": "/Reversi/static/js/main.00b2b2cb.chunk.js"
+  },
+  {
+    "revision": "b759777c76bb305a436e",
+    "url": "/Reversi/static/js/runtime~main.3ea17316.js"
+  },
+  {
+    "revision": "165094ba5495b533f27a",
+    "url": "/Reversi/static/js/2.bd3c1393.chunk.js"
+  },
+  {
+    "revision": "582aef09c013308a38eb25c86d6b7abf",
+    "url": "/Reversi/static/media/background.582aef09.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reversi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b40ecf1dfb8911c175b1d9ef60bec206",
+    "url": "/Reversi/index.html"
+  }
+];
